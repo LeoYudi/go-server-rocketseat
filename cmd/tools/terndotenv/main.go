838a6ff40cdb6f9011d5dbe52b369f39c46a2df8
Leonardo Higuti <lyhiguti@gmail.com>
2024-08-05 22:26:49 -0300

    first class

---
+++ cmd/tools/terndotenv/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"./internal/store/pgstore/migrations",
+		"--config",
+		"./internal/store/pgstore/migrations/tern.conf",
+	)
+
+	if err := cmd.Run(); err != nil {
+		log.Println(cmd)
+		panic(err)
+	}
+}
